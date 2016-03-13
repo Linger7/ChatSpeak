0b29f1c4da1f7f3b4b87e3bbb65f677217aa7e44
Linger7 <lingeric95@gmail.com>
2016-03-12 19:15:44 -0500

    Added user sign in Added encryption on passwords

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 var mysqlConnection = require('../utilities/mysqlConnection.js')
 var config = require('../config/config.js');
 
@@ -10,6 +11,36 @@ router.get('/', function(req, res, next) {
     //If not signed in then send register/login view
     res.render('registration/login', function(err, html) {
         res.send(router.generateResponseObject('<i class="fa fa-user"></i>  Sign In or Register', html));
+    });
+});
+
+router.post('/login', function(req, res, next){
+    var userName = req.body.inputUserName;
+    var password = req.body.inputPassword;
+
+    var connection = mysqlConnection();
+    connection.getConnection(function(err, connection) {
+        connection.query('SELECT password FROM user WHERE username = ?', [userName],function(err, rows, fields){
+            if(err){
+                router.sendError(res, 'Database issues, unable to login!');
+            } else {
+                var responseObject = {};
+                if(rows.length != 0) {
+                    var hashedPW = rows[0].password;
+                    bcrypt.compare(password, hashedPW, function (err, result) {
+                        if (result == true) {
+                            responseObject.status = 'Success';
+                        } else {
+                            responseObject.status = 'Wrong Password';
+                        }
+                        res.send(responseObject);
+                    });
+                } else {
+                    responseObject.status = 'Invalid User';
+                    res.send(responseObject);
+                }
+            }
+        });
     });
 });
 
@@ -38,35 +69,45 @@ router.post('/register', function(req, res, next){
     } else if(password === null || password.length < 6 || password.length > 12){
         res.send(404);
     }
-
-    var connection = mysqlConnection();
-    connection.getConnection(function(err, connection){
-        connection.query('INSERT INTO user (username, password, email, usergroup_uid, ip_address) VALUES (?, ?, ?, ?, ?)', [userName, password, email, config.defaults.usergroup, req.connection.remoteAddress],function(err, rows, fields){
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
             if(err){
-                if(err.code === 'ER_DUP_ENTRY'){
-                    var errorMessage = String(err.message);
-                    if(errorMessage.indexOf(userName) > -1){
-                        res.render('registration/error', {params: {message: 'The username ' + userName + ' is already taken!'}}, function(err, html) {
-                            res.send(router.generateResponseObject('<i class="fa fa-exclamation-triangle"></i> Oops something went wrong!', html));
-                        });
-                    } else if(errorMessage.indexOf(email) > -1){
-                        res.render('registration/error', {params: {message: 'The email address ' + email + ' is already taken!'}}, function(err, html) {
-                            res.send(router.generateResponseObject('<i class="fa fa-exclamation-triangle"></i> Oops something went wrong!', html));
-                        });
-                    } else {
-                        res.render('registration/error', {params: {message: err.message}}, function(err, html) {
-                            res.send(router.generateResponseObject('<i class="fa fa-exclamation-triangle"></i> Oops something went wrong!', html));
-                        });
-                    }
-                }
+                console.log(err);
+                router.sendError(res, 'Unable to encrypt password!');
             } else {
-                res.render('registration/success', {params: {username: userName}}, function(err, html) {
-                    res.send(router.generateResponseObject('<i class="fa fa-thumbs-up"></i> Congratulations!', html));
+                // Store hash in your password DB.
+                var connection = mysqlConnection();
+                connection.getConnection(function (err, connection) {
+                    connection.query('INSERT INTO user (username, password, email, usergroup_uid, ip_address) VALUES (?, ?, ?, ?, ?)', [userName, hash, email, config.defaults.usergroup, req.connection.remoteAddress], function (err, rows, fields) {
+                        if (err) {
+                            if (err.code === 'ER_DUP_ENTRY') {
+                                var errorMessage = String(err.message);
+                                if (errorMessage.indexOf(userName) > -1) {
+                                    router.sendError(res, 'The username ' + userName + ' is already taken!');
+                                } else if (errorMessage.indexOf(email) > -1) {
+                                    router.sendError(res, 'The email address ' + email + ' is already taken!');
+                                } else {
+                                    router.sendError(res, 'A database issue occurred! ' + err.message);
+                                }
+                            }
+                        } else {
+                            res.render('registration/success', {params: {username: userName}}, function (err, html) {
+                                res.send(router.generateResponseObject('<i class="fa fa-thumbs-up"></i> Congratulations!', html));
+                            });
+                        }
+                    });
                 });
             }
         });
     });
 });
+
+
+router.sendError = function(res, message){
+    res.render('registration/error', {params: {message: message}}, function (err, html) {
+        res.send(router.generateResponseObject('<i class="fa fa-exclamation-triangle"></i> Oops something went wrong!', html));
+    });
+};
 
 //Create JSON Object with the Modal Title and Body
 router.generateResponseObject = function(title, content){
